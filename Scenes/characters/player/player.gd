64ce0560f38dd45c38be121db465bc4af5f9ee2d
JevonLee <3328008568@qq.com>
2024-11-09 23:22:19 +0800

    5th

---
+++ Scenes/characters/player/player.gd
@@ -0,0 +1,5 @@
+class_name Player extends CharacterBody2D
+
+@export var current_tool : DataTypes.Tools
+
+var player_direction:Vector2
---
+++ Scripts/globals/date_types.gd
@@ -0,0 +1,10 @@
+class_name DataTypes
+
+enum Tools{ #使用枚举类型定义工具
+	None,
+	AxeWood,
+	TillGround,
+	WaterCrops,
+	PlantCorn,
+	PlantTomato
+}
